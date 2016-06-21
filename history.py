340d6a881c075354c86b6ee41666a9f8c281162e
Jon Egeland <audiobahn404@gmail.com>
2016-06-21 00:31:48 -0400

    Implement global event history. The last message sent across each topic can be retrieved by calling the remote procedure `meta.last_event`, with a single argument: the channel being queried. The result will be the exact message previously sent to that channel.

---
+++ history.py
@@ -0,0 +1,36 @@
+from twisted.internet.defer import inlineCallbacks
+
+from autobahn.twisted.wamp import ApplicationSession
+from autobahn.wamp.types import SubscribeOptions, PublishOptions
+
+HISTORY_SESSION_EVENT_MAP = {}
+
+# A Crossbar component for storing the last event sent across a channel. These
+# events can be retrieved through the `meta.last_event` procedure call, passing
+# the URI of the channel in question as a parameter.
+#
+# Currently, only the last event (one event) from the channel is remembered. As
+# soon as another event passes through the channel, the last event will be
+# replaced and can no longer be retrieved.
+class HistorySession(ApplicationSession):
+  @inlineCallbacks
+  def onJoin(self, details):
+    print("History session joined: {}".format(details))
+
+
+    # Automatically subscribe to all topics so that all events can be stored.
+    def store_message(msg, details=None):
+      print("Message received on {}".format(details.topic))
+      HISTORY_SESSION_EVENT_MAP[details.topic] = msg
+
+    sub = yield self.subscribe(store_message, '.', options=SubscribeOptions(match=u'wildcard', details_arg='details'))
+    print('Now tracking events from all topics.')
+
+
+    # Register the `meta.last_event` remote procedure.
+    def last_event(topic):
+      print("Got request for last event on: {}".format(topic))
+      return HISTORY_SESSION_EVENT_MAP.get(topic, 'no events available')
+
+    reg = yield self.register(last_event, 'meta.last_event')
+    print('Event history now available at `meta.last_event`.')
