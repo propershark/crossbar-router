b1d0d05557010c5369a790c9956964114956c87d
Jon Egeland <audiobahn404@gmail.com>
2016-06-30 18:26:36 -0400

    Use a multi-level dictionary to store events. This allows events to be discriminated based on topic *and* originator, meaning users can look up events sent by other entities on a topic. For example, a Vehicle may send updates to a Station channel. Using `last_event(<station_channel>, <vehicle_identifier>)`, that vehicle's last update will be returned.

--- history.py
+++ history.py
@@ -1,9 +1,17 @@
+from collections import defaultdict
+
 from twisted.internet.defer import inlineCallbacks
 
 from autobahn.twisted.wamp import ApplicationSession
 from autobahn.wamp.types import SubscribeOptions, PublishOptions
 
-HISTORY_SESSION_EVENT_MAP = {}
+
+# The two-layer map of past events. The first layer identifies the topic on
+# which the event was passed. The second layer identifies who sent the message.
+# From here, a complete history of the last thing any publisher has done can be
+# obtained.
+HISTORY_SESSION_EVENT_MAP = defaultdict(lambda: {})
+
 
 # A Crossbar component for storing the last event sent across a channel. These
 # events can be retrieved through the `meta.last_event` procedure call, passing
@@ -21,15 +29,17 @@ class HistorySession(ApplicationSession):
     # Automatically subscribe to all topics so that all events can be stored.
     def store_message(*args, **kwargs):
       details = kwargs.pop('details')
-      HISTORY_SESSION_EVENT_MAP[details.topic] = [args, kwargs]
+      HISTORY_SESSION_EVENT_MAP[details.topic][kwargs['originator']] = [args, kwargs]
+      print('Got message from {} on {}'.format(kwargs['originator'], details.topic))
 
     sub = yield self.subscribe(store_message, '.', options=SubscribeOptions(match=u'wildcard', details_arg='details'))
     print('Now tracking events from all topics.')
 
 
     # Register the `meta.last_event` remote procedure.
-    def last_event(topic):
-      return HISTORY_SESSION_EVENT_MAP.get(topic, 'no events available')
+    def last_event(topic, originator):
+      print('Looking up last message from {} on {}'.format(originator, topic))
+      return HISTORY_SESSION_EVENT_MAP.get(topic, {}).get(originator)
 
     reg = yield self.register(last_event, 'meta.last_event')
     print('Event history now available at `meta.last_event`.')
