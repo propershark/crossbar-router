7579bd0cb685ace4fcd69f5e89a4366cba82a8a0
Jon Egeland <audiobahn404@gmail.com>
2016-06-30 16:04:15 -0400

    History now passes args *and* kwargs. Granted, it's a two-element array with args and kwargs as arguments, but close enough.

--- history.py
+++ history.py
@@ -19,9 +19,9 @@ class HistorySession(ApplicationSession):
 
 
     # Automatically subscribe to all topics so that all events can be stored.
-    def store_message(msg, details=None):
-      print("Message received on {}".format(details.topic))
-      HISTORY_SESSION_EVENT_MAP[details.topic] = msg
+    def store_message(*args, **kwargs):
+      details = kwargs.pop('details')
+      HISTORY_SESSION_EVENT_MAP[details.topic] = [args, kwargs]
 
     sub = yield self.subscribe(store_message, '.', options=SubscribeOptions(match=u'wildcard', details_arg='details'))
     print('Now tracking events from all topics.')
@@ -29,7 +29,6 @@ class HistorySession(ApplicationSession):
 
     # Register the `meta.last_event` remote procedure.
     def last_event(topic):
-      print("Got request for last event on: {}".format(topic))
       return HISTORY_SESSION_EVENT_MAP.get(topic, 'no events available')
 
     reg = yield self.register(last_event, 'meta.last_event')
